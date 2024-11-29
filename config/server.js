1d755cc8c4a1fef493acc8043818986c9dd05e83
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2024-11-29 03:24:57 -0300

    database configuration

---
+++ config/server.js
@@ -0,0 +1,18 @@
+const port = 3003;
+const bodyParser = require('body-parser');
+const express = require('express');
+const server = express();
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.listen(port, function() {
+    console.log(`Backend running on port ${port}.`);
+});
+
+server.use(function(req, res, next) {
+    res.send('Welcome');
+    next();
+});
+
+
