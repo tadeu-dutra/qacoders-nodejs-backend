f036c1c62b4961d91d6837a20f4638172d1d4046
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2024-11-30 03:24:49 -0300

    update routes.js to add student endpoint

--- config/routes.js
+++ config/routes.js
@@ -5,7 +5,10 @@ module.exports = function(server) {
     const router = express.Router();
     server.use('/api', router);
 
-    router.route('/test').get(function(req, res, next) {
-        res.send('It works!');
-    });
+    // router.route('/test').get(function(req, res, next) {
+    //     res.send('It works!');
+    // });
+
+    const student = require('../api/student/student');
+    student.register(router, 'students');
 };
