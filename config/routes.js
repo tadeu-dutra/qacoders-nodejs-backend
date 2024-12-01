f165033d94601f933732104e4745caf9480b2d58
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2024-12-01 18:33:08 -0300

    update routes.js

--- config/routes.js
+++ config/routes.js
@@ -2,16 +2,20 @@ const express = require('express');
 
 module.exports = function(server) {
     
-    const router = express.Router();
+    const api = express.Router();
+    server.use('/api', api);
 
-    server.get('/test', (req, res) => {
-        res.send('Server is running!!!');
-    });
+    api.use('/status', (req, res) =>
+        res.send(`BACKEND is runner.`)
+    );
 
-    server.use('/api', router);
+    // server.use('/api', router);
     const studentService = require('../api/student/studentService.js');
-    studentService.register(router, '/students');
+    studentService.register(api, '/students');
 
     const userService = require('../api/user/userService.js');
-    userService.register(router, '/users');
+    userService.register(api, '/users');
+
+    // server.use(express.static(require('path').join(__dirname, '../public')));
+    api.use(express.static(require('path').join(__dirname, '../public')));
 };
